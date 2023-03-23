6064709c55fec4f33424e4e4a56691ec5d6ee0d5
ThalysAragao <nascimentogames132@gmail.com>
2023-03-23 15:33:51 -0300

    novas mensagens

---
+++ l2q18.py
@@ -0,0 +1,22 @@
+#Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
+
+print("\tData Válida\n")
+
+#Entrada de dados
+data = (input("Data: "))
+
+#Fatiamento e cálculo
+if int(data[0:2]) != 0  and int(data[0:2]) <= 31 :
+    if int(data[3:5]) != 0  and int(data[3:5]) <= 12 :
+        if int(data[6:11]) != 0 :
+            print("")
+            print("Data Válida")
+        else :
+            print("")
+            print("Data Inválida")
+    else :
+        print("")
+        print("Data Inválida")
+else:
+    print("")
+    print("Data Inválida")
